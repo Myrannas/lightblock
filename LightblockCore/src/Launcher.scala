006c9702fd1bd2a91157f4b6d5453a2879bfc315
Myrannas <pyroinc89@yahoo.com.au>
2012-05-27 21:42:58 +1000

    Created project.

--- LightblockCore/src/Launcher.scala
+++ LightblockCore/src/Launcher.scala
@@ -1,11 +1,8 @@
 /**
- * Created with IntelliJ IDEA.
  * User: michael
  * Date: 5/27/12
  * Time: 9:37 PM
- * To change this template use File | Settings | File Templates.
  */
-
 class Launcher {
 
 }
